1a92425cb06629fa25ac4d4b1c4c00c1b49a70c1
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 20:12:19 +0530

    Update s3_bkt.tf

--- s3_bkt.tf
+++ s3_bkt.tf
@@ -1,14 +1,14 @@
-provider "aws" {
-  region = "ap-south-1"  # Adjust your region
+resource "aws_s3_bucket" "abucket" {
+  bucket = "avibkt"
+  acl    = "private"
+ 
+  # Other S3 settings...
 }
-
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "hamribkt"  # Replace with your unique bucket name
-  tags = {
-    Name        = "MyS3Bucket"
-    Environment = "Development"
-  }
-
-  block_public_acls = false  # Allow ACLs on the bucket
-  block_public_policy = false  # Allow public bucket policies if needed
+ 
+resource "aws_s3_bucket_object" "newobject" {
+  bucket = aws_s3_bucket.sbucket.bucket
+  key    = "example-key1"
+  source = "myfile1.txt"
+  acl    = "private"
 }
+ 
