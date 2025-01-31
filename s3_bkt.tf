a32073d6a9582a3310e8d412cfffc5cd6ff366aa
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 22:08:38 +0530

    Update s3_bkt.tf

--- s3_bkt.tf
+++ s3_bkt.tf
@@ -1,14 +1,23 @@
+provider "aws" {
+  region = "ap-south-1"  # Adjust your region
+}
+
+# S3 Bucket Creation without acl argument
 resource "aws_s3_bucket" "sbucket" {
   bucket = "AVbkt"
-  acl    = "public"
- 
-  # Other S3 settings...
+  # Removed acl argument as it's deprecated
 }
- 
-resource "aws_s3_bucket_object" "newobject" {
+
+# Explicitly setting the ACL using aws_s3_bucket_acl
+resource "aws_s3_bucket_acl" "sbucket_acl" {
+  bucket = aws_s3_bucket.sbucket.bucket
+  acl    = "public-read"  # Set the ACL to public-read (this is typically the intended behavior for public access)
+}
+
+# Using aws_s3_object instead of aws_s3_bucket_object
+resource "aws_s3_object" "newobject" {
   bucket = aws_s3_bucket.sbucket.bucket
   key    = "example-key"
   source = "myfile.txt"
-  acl    = "public"
+  acl    = "public-read"  # Set the ACL to public-read (this allows public access to the object)
 }
- 
