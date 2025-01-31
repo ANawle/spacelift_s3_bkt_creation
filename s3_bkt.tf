b76b2279bbfea0dc75fad0d185fb1fe4b118f185
ANawle <149271449+ANawle@users.noreply.github.com>
2025-01-31 21:58:18 +0530

    Update s3_bkt.tf

--- s3_bkt.tf
+++ s3_bkt.tf
@@ -1,4 +1,13 @@
 resource "aws_s3_bucket" "sbucket" {
-  bucket = "AVIbakt"
+  bucket = "avibkt"
   acl    = "private"
+ 
+  # Other S3 settings...
 }
+ 
+resource "aws_s3_bucket_object" "newobject" {
+  bucket = aws_s3_bucket.sbucket.bucket
+  key    = "example-key"
+  source = "myfile.txt"
+}
+ 
